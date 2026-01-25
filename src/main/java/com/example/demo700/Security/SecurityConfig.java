3b79824bc0e9f22a6e67315f69770c2267b5419c
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-01-25 23:09:33 +0600

    Updated files before pulling latest changes

--- src/main/java/com/example/demo700/Security/SecurityConfig.java
+++ src/main/java/com/example/demo700/Security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo700.Security;
 
+
+import org.springframework.boot.security.autoconfigure.SecurityProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -53,4 +57,13 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    @Bean
+    public FilterRegistrationBean<MultipartFilter> multipartFilter() {
+        FilterRegistrationBean<MultipartFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MultipartFilter());
+        
+        return registrationBean;
+    }
+    
 }
