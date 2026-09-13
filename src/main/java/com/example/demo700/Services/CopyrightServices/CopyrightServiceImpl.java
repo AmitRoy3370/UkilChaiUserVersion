4bc66f24221df79c12839bf7c893f1eb2d414155
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2026-09-13 20:30:05 +0600

    Updated files before pulling latest changes

--- src/main/java/com/example/demo700/Services/CopyrightServices/CopyrightServiceImpl.java
+++ src/main/java/com/example/demo700/Services/CopyrightServices/CopyrightServiceImpl.java
@@ -1002,7 +1002,7 @@ public class CopyrightServiceImpl implements CopyrightService {
 
 					}
 
-					return processRepository.findAllById(copyrightIds).stream().collect(
+					return processRepository.findByCopyrightId(copyrightIds).stream().collect(
 							Collectors.toMap(CopyrightRegistrationProcess::getCopyrightId, Function.identity()));
 
 				}, executor);
